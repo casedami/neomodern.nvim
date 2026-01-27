7b7e1f5d5511b1d1ec6011819b7459b1c82b782b
Casey Miller <cd.miller1230@gmail.com>
2026-01-27 13:59:24 -0700

    chore(extras): auto-gen

--- extras/fish/roseprime.fish
+++ extras/fish/roseprime.fish
@@ -5,14 +5,14 @@
 
 # use in ~/.config/fish/conf.d/
 
-# preferred bg: 15151a
+# preferred bg: 141517
 
 ### Full palette. 
 ### Colors defined in neomdern/palette/roseprime.lua
 set -g alt 9FB6D0
 set -g constant 96aff2
-set -g comment 67606A
-set -g fg C5B6C6
+set -g comment 666068
+set -g fg C4B6C5
 set -g func c4959c
 set -g keyword 5f86b0
 set -g number A18877
@@ -22,32 +22,32 @@ set -g str c9aa95
 set -g type 9bbdb8
 
 # Syntax Highlighting Colors
-set -g fish_color_normal C5B6C6
+set -g fish_color_normal C4B6C5
 set -g fish_color_command c4959c
 set -g fish_color_keyword 5f86b0
 set -g fish_color_quote c9aa95
 set -g fish_color_redirection 96aff2
 set -g fish_color_end 9594D4
 set -g fish_color_error DF8386
-set -g fish_color_param C5B6C6
+set -g fish_color_param C4B6C5
 set -g fish_color_valid_path 9bbdb8
 set -g fish_color_option 9FB6D0
-set -g fish_color_comment 67606A
-set -g fish_color_selection --background=1D1D22
+set -g fish_color_comment 666068
+set -g fish_color_selection --background=1C1D1F
 set -g fish_color_operator 9594D4
 set -g fish_color_escape 5f86b0
-set -g fish_color_autosuggestion 67606A
+set -g fish_color_autosuggestion 666068
 set -g fish_color_cwd c9aa95
 set -g fish_color_hostname A18877
 set -g fish_color_status DF8386
 set -g fish_color_cancel A18877
-set -g fish_color_search_match --background=1D1D22
+set -g fish_color_search_match --background=1C1D1F
 
 # Completion Pager Colors
 set -g fish_pager_color_progress 9FB6D0
 set -g fish_pager_color_prefix 9bbdb8
-set -g fish_pager_color_completion C5B6C6
-set -g fish_pager_color_description 67606A
+set -g fish_pager_color_completion C4B6C5
+set -g fish_pager_color_description 666068
 set -g fish_pager_color_selected_prefix 9FB6D0
 set -g fish_pager_color_selected_completion 9FB6D0
-set -g fish_pager_color_selected_background --background=1D1D22
+set -g fish_pager_color_selected_background --background=1C1D1F
